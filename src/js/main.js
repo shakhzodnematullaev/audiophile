3035519db605da03329f0aef4eb3dc14752bd4e3
shakhzodnematullaev <shakhzodnematullaev@gmail.com>
2021-06-26 22:36:56 +0500

    add checkout section

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,10 @@ let deviceitem2 = document.querySelector(".devices-burger__list-item2");
 let deviceitem3 = document.querySelector(".devices-burger__list-item3");
 let body = document.querySelector("body");
 
+let cart = document.querySelector(".cart-link");
+let modal = document.querySelector(".modal");
+let modalspan =document.querySelector(".modal-span");
+
 burger.addEventListener("click", function() {
     burger.classList.toggle("hamburger--active");
     device.classList.toggle("devices-burger__container--active");
@@ -13,3 +17,16 @@ burger.addEventListener("click", function() {
     deviceitem3.classList.toggle("devices-burger__list-item3--active");
     body.classList.toggle("body");
 });
+
+cart.addEventListener("click", function() {
+    modal.classList.toggle("modal--active");
+    body.classList.toggle("body");
+    modalspan.classList.toggle("modal-span--active");
+})
+
+window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+        modal.classList.remove("modal--active")
+        body.classList.remove("body");
+    }
+})
